d6ace7cd57e616b2209f69b4f9ec73607f581539
Bryan Riel <bryanvriel@gmail.com>
2020-05-07 14:59:35 +0800

    Demonstration of new tseries return and warnings.

--- helheim-tseries_decomp.py
+++ helheim-tseries_decomp.py
@@ -6,8 +6,8 @@ import iceutils as ice
 import sys
 
 ## Set up combined hdf5 stack
-fpath='/Users/lizz/Documents/Research/Gld-timeseries/Stack/'
-hel_stack = ice.MagStack(files=[fpath+'vx.h5', fpath+'vy.h5'])
+#fpath='/Users/lizz/Documents/Research/Gld-timeseries/Stack/'
+hel_stack = ice.MagStack(files=['vx.h5', 'vy.h5'])
 data_key = 'igram' # B. Riel convention for access to datasets in hdf5 stack
 
 ## Extract time series at selected points
@@ -54,14 +54,21 @@ def build_collection(dates):
     # Done
     return collection
 
-# First convert the time vector to a list of datetime
+# Create an evenly spaced time array for time series predictions
+t_grid = np.linspace(hel_stack.tdec[0], hel_stack.tdec[-1], 1000)
+
+# First convert the time vectors to a list of datetime
 dates = ice.tdec2datestr(hel_stack.tdec, returndate=True)
+dates_grid = ice.tdec2datestr(t_grid, returndate=True)
 
 # Build the collection
 collection = build_collection(dates)
 
-# Instantiate a model
+# Instantiate a model for inversion
 model = ice.tseries.Model(dates, collection=collection)
+
+# Instantiate a model for prediction
+model_pred = ice.tseries.Model(dates_grid, collection=collection)
 
 ## Access the design matrix for plotting
 G = model.G
@@ -70,23 +77,60 @@ G = model.G
 # plt.ylabel('Amplitude')
 # plt.show()
 
-# Create a ridge regression solver that damps out the transient spline coefficients
-ridge = ice.tseries.select_solver('ridge', reg_indices=model.itransient, penalty=2)
+# First create a solver with a minimum data threshold of 200 (i.e., we require at least
+# 200 valid data points to perform an inversion). This should file for all four time series
+ridge = ice.tseries.select_solver('ridge', reg_indices=model.itransient, penalty=2, n_min=200)
 
-# Perform inversion to get coefficient vector and coefficient covariance matrix
-m, Cm = ridge.invert(model.G, series[0]) # fit near-terminus (series[0]) first
+# Un-comment the following two lines to ensure warnings are printed out every time
+#import warnings
+#warnings.simplefilter('always', UserWarning)
 
-# Model will perform predictions
-pred = model.predict(m)
+# Loop over time series
+for i in range(len(series)):
+    # This should raise a warning and return a FAIL status (=0)
+    status, m, Cm = ridge.invert(model.G, series[i])
+    if status == ice.FAIL:
+        print('Unsuccessful inversion')
 
-print(len(pred['full']), len(series[0]))
-print(sum(np.isnan(pred['full'])), sum(np.isnan(series[0])))
-print(np.nanmean(pred['full']), np.nanmean(series[0]))
+# Create ridge regression solver that damps out the transient spline coefficients
+#solver = ice.tseries.select_solver('ridge', reg_indices=model.itransient, penalty=2)
+solver = ice.tseries.select_solver('lasso', reg_indices=model.itransient, penalty=1.5, rw_iter=1)
 
-## Plotting
-# fig, ax = plt.subplots(figsize=(12,6))
-# ax.plot(hel_stack.tdec, series[0], '.')
-# ax.plot(hel_stack.tdec, pred['full'])
-# ax.set_xlabel('Year')
-# ax.set_ylabel('Velocity')
-# plt.show()
+# Loop over time series
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12,6))
+for i in range(len(series)):
+
+    # Perform inversion to get coefficient vector and coefficient covariance matrix
+    status, m, Cm = solver.invert(model.G, series[i]) # fit near-terminus (series[0]) first
+    assert status == ice.SUCCESS, 'Failed inversion'
+    
+    # Model will perform predictions
+    pred = model_pred.predict(m)
+
+    # Separate out seasonal (short-term) and secular + transient (long-term) signals
+    short_term = pred['seasonal']
+    long_term = pred['secular'] + pred['transient']
+
+    # Remove long-term signals from data
+    series_short_term = series[i] - np.interp(hel_stack.tdec, t_grid, long_term)
+    
+    #print(len(pred['full']), len(series[0]))
+    #print(sum(np.isnan(pred['full'])), sum(np.isnan(series[0])))
+    #print(np.nanmean(pred['full']), np.nanmean(series[0]))
+    
+    # Plot long-term
+    ax1.plot(hel_stack.tdec, series[i], '.')
+    ax1.plot(t_grid, long_term, label=labels[i])
+
+    # Plot short-term (add estimated bias (m[0]) for visual clarity)
+    ax2.plot(hel_stack.tdec, series_short_term + m[0], '.')
+    ax2.plot(t_grid, short_term + m[0], label=labels[i])
+
+for ax in (ax1, ax2):
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Velocity')
+
+ax1.set_title('Multi-annual fit')
+ax2.set_title('Seasonal fit')
+
+plt.show()
